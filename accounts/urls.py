b266e9edbc4e7fc0b45d9eda38c3062aa9344b5d
Kyabetsu8931Ya31 <yuuki.20020217@gmail.com>
2022-03-29 22:35:19 +0900

    fix login form

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,13 +2,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView,LogoutView
 from django.urls import path
 from django.views.generic import CreateView
+from .views import signup
 
 urlpatterns = [
-    path('signup/',CreateView.as_view(
-        template_name='accounts/signup.html',
-        form_class=UserCreationForm,
-        success_url='/',
-    ),name='signup'),
+    path('signup/',signup,name='signup'),
     path('login/',LoginView.as_view(
         redirect_authenticated_user=True,
         template_name='accounts/login.html'
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import SignUpForm
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignUpForm()
+
+    return render(request,'accounts/signup.html',{'form':form})
